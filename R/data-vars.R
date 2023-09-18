e37b9e3ce4f5fa24eec7433844ce8d662b10ef5a
Adam <cmattsson@bcgsc.ca>
2023-09-18 15:21:32 -0700

    Adding docs for global variable (resolving warning from devtools:check)

--- R/data-vars.R
+++ R/data-vars.R
@@ -1,4 +1,13 @@
+#' @title Coding Class
+#' 
+#' @description Coding classes as a vector of characters.
+#' 
+#' @details This object is called by [GAMBLR.data::get_coding_ssm] and [GAMBLR.data::assign_cn_to_ssm].
+#' 
 #' @export
+#' 
+#' @noRd
+#' 
 coding_class = c("Frame_Shift_Del", "Frame_Shift_Ins", "In_Frame_Del", 
                  "In_Frame_Ins", "Missense_Mutation", "Nonsense_Mutation", 
                  "Nonstop_Mutation", "Silent", "Splice_Region", 
