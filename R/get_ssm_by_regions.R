bfda63744ccdb33fb6d4af045e7c129139167055
vladimirsouza <vladimirbcs@gmail.com>
2023-09-27 23:05:48 -0700

    Add changes to get_ssm_by_regions

--- R/get_ssm_by_regions.R
+++ R/get_ssm_by_regions.R
@@ -3,14 +3,16 @@
 #' @description Efficiently retrieve all mutations across a range of genomic regions.
 #'
 #' @details This function internally calls [GAMBLR.data::get_ssm_by_region] to retrieve SSM calls for the specified regions.
+#' 
+#' Unlike its counterpart version from `GAMBLR.results`, this function does not have the 
+#'   `basic_columns` argument, since the bundled MAF, from which it fetches data, already 
+#'   contains only the 45 basic columns, plus the additional "Pipeline" column (46 in total).
 #'
 #' @param regions_vector Either provide a vector of regions in the chr:start-end format. 
 #' If this parameter is provided, the function will auto set `use_name_column` to FALSE.
 #' @param regions_bed Provide a data frame in BED format with the coordinates you want to retrieve (recommended). 
 #' This parameter can also accept an additional column with region names that will be added to the return if `use_name_column = TRUE` 
 #' @param streamlined If set to TRUE (default) only 3 columns will be kept in the returned data frame (start, sample_id and region_name). 
-#' Set to FALSE to return 6 columns. See `basic_columns` for more information on what columns to include in the return.
-#' @param basic_columns Set to TRUE for returning a maf with standard 45 columns. Default is FALSE. If TRUE, `streamlined` and `use_name_column` will be ignored.
 #' @param use_name_column If your bed-format data frame has a name column (must be named "name") these can be used to name your regions. 
 #' This parameter is not accepted if `regions_vector` is provided.
 #' @param projection Obtain variants projected to this reference (one of grch37 or hg38).
@@ -37,7 +39,7 @@
 get_ssm_by_regions = function(regions_vector,
                               regions_bed,
                               streamlined = TRUE,
-                              basic_columns = FALSE,
+                              # basic_columns = FALSE,
                               use_name_column = FALSE,
                               projection = "grch37",
                               min_read_support = 3,
@@ -49,11 +51,6 @@ get_ssm_by_regions = function(regions_vector,
   
   #check if any invalid parameters are provided
   check_excess_params(...)
-  
-  #streamline parameter combinations
-  if(basic_columns){
-    streamlined = FALSE
-  }
   
   bed2region = function(x){
     paste0(x[1], ":", as.numeric(x[2]), "-", as.numeric(x[3]))
@@ -69,7 +66,7 @@ get_ssm_by_regions = function(regions_vector,
     use_name_column = FALSE #override behavior of this parameter if a region vector is provided.
     regions = regions_vector
   }
-
+  
   if(verbose){
     print(regions)  
   }
@@ -80,13 +77,7 @@ get_ssm_by_regions = function(regions_vector,
                                                                            min_read_support = min_read_support,
                                                                            verbose = FALSE, #force to FALSE, suppressing noisy output
                                                                            ...)})
-
-  #return the standard 45 columns (default)
-  if(basic_columns){
-    print("bind_rows")
-    return(bind_rows(region_mafs))
-  }
-
+  
   #deal with region names
   if(!use_name_column){
     rn = regions
@@ -101,13 +92,13 @@ get_ssm_by_regions = function(regions_vector,
   unnested_df = tibbled_data %>%
     unnest_longer(region_mafs)
   
-  #return only 3 columns, if streamlined or 6 columns if FALSE (and basic_columns = FALSE)
+  #return only 3 columns if streamlined is TRUE, or 46 columns otherwise.
   if(streamlined){
     unlisted_df = mutate(unnested_df, start = region_mafs$Start_Position, sample_id = region_mafs$Tumor_Sample_Barcode) %>%
       dplyr::select(start, sample_id, region_name)
   }else{
-    unlisted_df = mutate(unnested_df, Chromosome = region_mafs$Chromosome, End_Position = region_mafs$End_Position, Start_Position = region_mafs$Start_Position, Tumor_Sample_Barcode = region_mafs$Tumor_Sample_Barcode) %>%
-      dplyr::select(Chromosome, Start_Position, End_Position, Tumor_Sample_Barcode, region_name)
+    print("bind_rows")
+    return(bind_rows(region_mafs))
   }
   return(unlisted_df)
 }
