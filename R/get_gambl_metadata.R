f00cb6cad2af74028878809688b261206b428124
vladimirsouza <vladimirbcs@gmail.com>
2024-03-05 16:56:58 -0800

    Fix get_gambl_metadata

--- R/get_gambl_metadata.R
+++ R/get_gambl_metadata.R
@@ -7,7 +7,7 @@
 #' It relies on the bundled metadata in this package.
 #'
 #' @param seq_type_filter Specify the seq type you want to return metadata for.
-#' Default is both genome and capture (all samples).
+#' Default is "genome".
 #' @param ... Any additional parameters.
 #'
 #' @return A data frame with metadata, tailored for user without GSC access.
@@ -56,13 +56,13 @@
 #' #return metadata for genome and capture samples
 #' all_meta = get_gambl_metadata(seq_type_filter = c("genome", "capture"))
 #'
-get_gambl_metadata = function(seq_type_filter = c("genome", "capture"),
+get_gambl_metadata = function(seq_type_filter = "genome",
                               ...){
 
   #check if any invalid parameters are provided
   check_excess_params(...)
 
   message("Using the bundled metadata in GAMBLR.data...")
-  return(GAMBLR.data::gambl_metadata %>%
+  return(GAMBLR.data::sample_data$meta %>%
            dplyr::filter(seq_type %in% seq_type_filter))
 }
