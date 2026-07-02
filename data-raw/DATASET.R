d2ce4c15b94e6a7b418bfba5479322bff8ef0e71
gbanco <giulianobanco4@gmail.com>
2026-07-02 10:19:53 -0700

    Correct @format lines

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -541,12 +541,10 @@ gencode_to_symbol <- system.file(
 usethis::use_data(gencode_to_symbol, overwrite = TRUE)
 
 # BED panel of adaptive sampling targets
-grch37_adaptive_sampling_bed = system.file("extdata","adaptive_sampling_targets.grch37.bed",package="GAMBLR.data") %>%
+grch37_adaptive_sampling_targets = system.file("extdata","adaptive_sampling_targets.grch37.bed",package="GAMBLR.data") %>%
   read_tsv()
+usethis::use_data(grch37_adaptive_sampling_targets, overwrite = TRUE)
 
-usethis::use_data(grch37_aglydif_bed, overwrite = TRUE)
-
-hg38_adaptive_sampling_bed = system.file("extdata","adaptive_sampling_targets.hg38.bed",package="GAMBLR.data") %>%
+hg38_adaptive_sampling_targets = system.file("extdata","adaptive_sampling_targets.hg38.bed",package="GAMBLR.data") %>%
   read_tsv()
-
-usethis::use_data(hg38_adaptive_sampling_bed, overwrite = TRUE)
+usethis::use_data(hg38_adaptive_sampling_targets, overwrite = TRUE)
